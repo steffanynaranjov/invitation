95be7092af147dab92ccafffe2d8afc600e19728
steffanynaranjov <steffany.naranjo@gmail.com>
2024-01-29 16:01:52 -0500

    pushing code new

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,34 @@
+'use client'
+
+import { useState } from 'react';
 import styles from './Home.module.css';
 import Image from 'next/image';
 
 export default function Home() {
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
     return (
       <main className={styles.main}>
         <div className={styles.sobre}>
-        <div className={styles.verticalLine}></div>
         <a
-        className={styles.homepage}
-        href={'/Home'}
-        rel="noopener noreferrer"
-      >
-            <Image
-            src={'/logo.png'}
+          className={styles.homepage}
+          href="/Home"
+          rel="noopener noreferrer"
+          onClick={handleClick}
+        >
+          <Image
+            src="/logo.png"
             width={300}
             height={300}
-            alt={'image map'}
-            className={styles.logo}
-            />
-            </a>
-                <div className={styles.verticalLineB}></div>
+            alt="image map"
+            className={`${styles.logo} ${isClicked ? styles.spin : ''}`}
+          />
+        </a>
         </div>
       </main>
     );
